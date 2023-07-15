0fe04ff0c1b1b2244887253b0f3cf4f0e0e798aa
Amanda Duva <amanda.j.duva@gmail.com>
2023-07-14 20:32:01 -0400

    fixed error with env and completed apis

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,16 +16,15 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
-
-router.post('/', async (req, res) => {
-  // create a new category
   try {
-    const categoryData = Category.create({
-      category_name: req.body.category_name
+    const categoryData = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
   } catch (error) {
@@ -33,8 +32,28 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products including its associated Category and Tag data
   Product.findAll({
-    attributes: ['id', 'product_name', 'price', 'stock'],
     include: [
       {
         model: Category,
@@ -15,7 +14,7 @@ router.get('/', (req, res) => {
       },
       {
         model: Tag,
-        attributes: ['tag_name']
+        through: ProductTag
       }
     ]
   })
@@ -33,7 +32,6 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'product_name', 'price', 'stock'],
     include: [
       {
         model: Category,
@@ -41,7 +39,7 @@ router.get('/:id', (req, res) => {
       },
       {
         model: Tag,
-        attributes: ['tag_name']
+        through: ProductTag
       }
     ]
   })
@@ -68,13 +66,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    category_id: req.body.category_id,
-    tagIds: req.body.tagIds
-    })
+  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
